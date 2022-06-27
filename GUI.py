111b6cf995f4b43775ec7164f07a476da2c87eab
Lev111111 <leviiloviee@gmail.com>
2022-06-27 19:45:32 +0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -24,38 +24,36 @@ def openfile():
     filepath = filedialog.askopenfile(title="Select file")
     print(filepath.read())
 
-def select_files():
-    filetypes = (
-        ('IMG files', '*.jpg ' + '*,jpeg ' + '*.png '),
-        # ('IMG files', '*.jpg'),
-        ('All files', '*.*')
-    )
+# def select_files():
+filetypes = (
+    ('IMG files', '*.jpg ' + '*,jpeg ' + '*.png '),
+    # ('IMG files', '*.jpg'),
+    ('All files', '*.*')
+)
 
-    filenames = fd.askopenfilenames(
-        title='Open files',
-        initialdir='/',
-        filetypes=filetypes)
+filenames = fd.askopenfilenames(
+    title='Coloring utility/open file',
+    initialdir='/',
+    filetypes=filetypes)
 
-    showinfo(
-        title='Selected Files',
-        message=filenames
-    )
+# showinfo(
+#     title='Selected Files',
+#     message=filenames
+# )
 
-    curDir = os.getcwd()
-    fn = curDir
-
-    print(filenames[0])
-    downIMG = ImageTk.PhotoImage(Image.open(filenames[0]).resize((320, 240)))
-    downIMGLable = Label(image = downIMG)
-    downIMGLable.grid(row = 8, column = 1)
-    
+curDir = os.getcwd()
+fn = curDir
 
 root = Tk()
+print(filenames[0])
+downIMG = ImageTk.PhotoImage(Image.open(filenames[0]).resize((320, 240)))
+downIMGLable = Label(image = downIMG)
+# downIMGLable.grid(row = 8, column = 1)
+    
+
 root.title("Coloring utility")
 root.geometry("1280x720")
 root.resizable(width = False, height = False)
-
-fileOpen = Button(text = "File", command = select_files)
 
 pR = Entry(text = "Red porogue")
 pG = Entry(text = "Green porogue")
@@ -69,20 +67,18 @@ pBtext = Label(text = "blue porogue:")
 
 result = Button(text = "Result")
 
-fileOpen.grid(row = 2, column = 1)
+result.grid(row = 5, column = 2)
 
-result.grid(row = 7, column = 1)
+pR.grid(row = 1, column = 2)
+pG.grid(row = 2, column = 2)
+pB.grid(row = 3, column = 2)
 
-pR.grid(row = 3, column = 2)
-pG.grid(row = 4, column = 2)
-pB.grid(row = 5, column = 2)
+# downIMG = ImageTk.PhotoImage(Image.open("/Users/lev/Documents/GitHub/Kirill_Images/icon.jpg").resize((320, 240)))
+# downIMGLable = Label(image = downIMG)
+downIMGLable.grid(row = 6, column = 1)
 
-downIMG = ImageTk.PhotoImage(Image.open("/Users/lev/Documents/GitHub/Kirill_Images/icon.jpg").resize((320, 240)))
-downIMGLable = Label(image = downIMG)
-downIMGLable.grid(row = 8, column = 1)
-
-pRtext.grid(row = 3, column = 1)
-pGtext.grid(row = 4, column = 1)
-pBtext.grid(row = 5 , column = 1)
+pRtext.grid(row = 1, column = 1)
+pGtext.grid(row = 2, column = 1)
+pBtext.grid(row = 3 , column = 1)
 
 root.mainloop()
