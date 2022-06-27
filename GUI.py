1cac77e89a8efb7ad499f437183174666288a290
Lev111111 <leviiloviee@gmail.com>
2022-06-27 12:19:13 +0700

    test

--- GUI.py
+++ GUI.py
@@ -1,13 +1,3 @@
-import tkinter
+import tk
 
-root = Tk()
-root["bg"] = "#fafafa"
-root.title = "Coloring utility"
-root.wm_attributes("-alpha", 0.7)
-root.geometry("640x480")
-root.resizable(width = False, height = False)
-
-canvas = Canvas(root, height = 480, width = 640)
-canvas.pack()
-
-root.mainloop()
+label = tk.Label(text='Hello world')
