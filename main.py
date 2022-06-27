beea3b00ccc5453b7c856aa4971833594985e6c8
Lev111111 <leviiloviee@gmail.com>
2022-06-27 09:11:34 +0700

    test

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from PIL import Image
 
-image_name = input("Print image name: ")
-# image_name = "icon.jpg"
-
+# Открытие изображениия
+# image_name = input("Print image name: ")
+image_name = "icon.jpg"
 image = Image.open(image_name)
-# image = Image.new("RGBA", [640, 480], color = "black")
+
+# Вывод параметров изображения
 print("Image: ", image.format, image.size, image.mode)
 
+# Работа с изображением
 for i in range(0, image.size[0]):
     for j in range(0, image.size[1]):
-        if image.getpixel((i, j)) > (0, 0, 0) and image.getpixel((i, j)) < (100, 100, 100):
+        if image.getpixel((i, j)) == (0, 0, 0):
+            image.putpixel((i, j), (1, 1, 1))
+        if image.getpixel((i,j)) > (0, 0, 0):
             image.putpixel((i, j), (255, 255, 255))
 
-image.save("tests/image6.png")
+# Сохранение и показ изображения
+image.save("tests/image8.png")
 image.show()
