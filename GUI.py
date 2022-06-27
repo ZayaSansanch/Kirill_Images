cbd8ba223f84567f70fd240dfa36752b829fd5e7
Lev111111 <leviiloviee@gmail.com>
2022-06-27 14:36:24 +0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,10 +1,11 @@
 from cProfile import label
 from tkinter import *
 from tkinter import filedialog
-from unittest import result
-from PIL import Image
+from PIL import Image, ImageTk
 # from PIL import ImageFilter
 
+downIMG = ""
+downIMGLable = ""
 image = ""
 filepath = ""
 pixel = (0, 0, 0)
@@ -14,41 +15,54 @@ pR = 0
 pG = 0
 pb = 0
 
-def openSave():
-    image.save("result.png")
-    image.show()
-
 def openfile():
-    filepath = filedialog.askopenfilename()
-    image = Image.open("icon.jpg")
-
-def result():
-    openSave()
+    filepath = filedialog()
 
 root = Tk()
 root.title("Coloring utility")
 root.geometry("1280x720")
 root.resizable(width = False, height = False)
 
-fileOpen = Button(text = "File", command=openfile)
+fileOpen = Button(text = "File", command = openfile)
 
 pR = Entry(text = "Red porogue")
 pG = Entry(text = "Green porogue")
 pB = Entry(text = "blue porogue")
 
-pRtext = label("Red porogue:")
-pGtext = label("Green porogue:")
-pBtext = label("blue porogue:")
+sizeH = Entry(text = "Image height")
+sizeW = Entry(text = "Image width")
 
-result = Button(text = "result", command = result)
+pRtext = Label(text = "Red porogue:")
+pGtext = Label(text = "Green porogue:")
+pBtext = Label(text = "blue porogue:")
+
+sizeHtext = Label(text = "Image height:")
+sizeWtext = Label(text = "Image width:")
+
+def showImages():
+    downIMG = ImageTk.PhotoImage(Image.open("icon.jpg").resize((320, 240), Image.ANTIALIAS))
+    downIMGLable = Label(image = downIMG)
+    downIMGLable.grid(row = 8, column = 1)
+
+result = Button(text = "Result", command = showImages)
+
+
 
 fileOpen.grid(row = 1, column = 1)
-pR.grid(row = 2, column = 2)
-pG.grid(row = 3, column = 2)
-pB.grid(row = 4, column = 2)
-pRtext.grid(row = 2, column = 1)
-pGtext.grid(row = 3, column = 1)
-pBtext.grid(row = 4, column = 1)
-result.grid(row = 5, column = 1)
+
+result.grid(row = 7, column = 1)
+
+pR.grid(row = 4, column = 2)
+pG.grid(row = 5, column = 2)
+pB.grid(row = 6, column = 2)
+sizeH.grid(row = 2, column = 2)
+sizeW.grid(row = 3, column = 2)
+
+pRtext.grid(row = 4, column = 1)
+pGtext.grid(row = 5, column = 1)
+pBtext.grid(row = 6, column = 1)
+sizeHtext.grid(row = 2, column = 1)
+sizeWtext.grid(row = 3, column = 1)
+
 
 root.mainloop()
