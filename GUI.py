23257c6b5a0230b52aab77bc386a89901f34d049
Lev111111 <leviiloviee@gmail.com>
2022-06-27 12:26:30 +0700

    test

--- GUI.py
+++ GUI.py
@@ -1,14 +1,12 @@
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-
-class Container(GridLayout):
-    pass
-
-class MainApp(App):
-    def build(self):
-        self.title = 'Coloring utility'
-        return Container()
-
-if __name__ == "__main__":
-    app = MainApp()
-    app.run()
+import wx
+ 
+class Window(wx.Frame):
+    def __init__(self, title):
+        super().__init__(parent = None, title = title)
+        self.panel = wx.Panel(self)
+ 
+        self.Show()
+         
+app = wx.App()
+window = Window("Coloring utility")
+app.MainLoop()
