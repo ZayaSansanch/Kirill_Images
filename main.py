aba9e3481952704b6bf879eb779a5591889cd408
Lev111111 <leviiloviee@gmail.com>
2022-06-27 11:34:13 +0700

    test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from PIL import ImageFilter
 
 # Переменные смен цвета
 pixel = (0, 0, 0)
@@ -8,10 +9,11 @@ pR = 0
 pG = 0
 pb = 0
 
-# Открытие изображениия
+# Открытие изображениия и применениие фильтров
 image_name = input("Print image name: ")
 # image_name = "icon.jpg"
 image = Image.open(image_name)
+image = image.filter(ImageFilter.SHARPEN)
 
 # Запрашиваю параметры парогов смены цвета
 pR = int(input("Parogue for red channel: "))
@@ -24,27 +26,27 @@ print("Parogue (RGB): ", pR, pG, pB)
 
 # Работаю с изображением
 print("Working with image")
-for i in range(0, image.size[0]):
-    for j in range(0, image.size[1]):
-        # Пиксели цвета (0, 0, 0) крашу в (1, 1, 1)
-        if image.getpixel((i, j)) == (0, 0, 0): image.putpixel((i, j), (1, 1, 1))
+# for i in range(0, image.size[0]):
+#     for j in range(0, image.size[1]):
+#         # Пиксели цвета (0, 0, 0) крашу в (1, 1, 1)
+#         if image.getpixel((i, j)) == (0, 0, 0): image.putpixel((i, j), (1, 1, 1))
         
-        # Крашу линии
-        pixel = image.getpixel((i, j))
-        if j < image.size[1] - 1:
-            pixelRight = image.getpixel((i, j + 1))
+#         # Крашу линии
+#         pixel = image.getpixel((i, j))
+#         if j < image.size[1] - 1:
+#             pixelRight = image.getpixel((i, j + 1))
         
-        if abs(pixel[0] - pixelRight[0]) > pR: cmena = True
-        if abs(pixel[1] - pixelRight[1]) > pG: cmena = True
-        if abs(pixel[2] - pixelRight[2]) > pB: cmena = True
+#         if abs(pixel[0] - pixelRight[0]) > pR: cmena = True
+#         if abs(pixel[1] - pixelRight[1]) > pG: cmena = True
+#         if abs(pixel[2] - pixelRight[2]) > pB: cmena = True
 
-        if cmena == True:
-            image.putpixel((i, j), (0, 0, 0))
-            cmena = False
+#         if cmena == True:
+#             image.putpixel((i, j), (0, 0, 0))
+#             cmena = False
 
-        # Пиксели цвета > (0, 0, 0) крашу в (255, 255, 255)
-        if image.getpixel((i, j)) > (0, 0, 0): image.putpixel((i, j), (255, 255, 255))
+#         # Пиксели цвета > (0, 0, 0) крашу в (255, 255, 255)
+#         if image.getpixel((i, j)) > (0, 0, 0): image.putpixel((i, j), (255, 255, 255))
 
 # Сохранение и показ изображения
-image.save(image_name + "_result.png")
+image.save("result5.png")
 image.show()
