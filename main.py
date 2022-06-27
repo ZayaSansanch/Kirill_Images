48156aee7903940c9b784273104d99568198cc36
Lev111111 <leviiloviee@gmail.com>
2022-06-27 10:38:01 +0700

    test

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ image_name = "icon.jpg"
 image = Image.open(image_name)
 
 # Запрашиваю параметры парогов смены цвета
-pR = input("Parogue for red channel: ")
-pG = input("Parogue for green channel: ")
-pB = input("Parogue for blue channel: ")
+pR = int(input("Parogue for red channel: "))
+pG = int(input("Parogue for green channel: "))
+pB = int(input("Parogue for blue channel: "))
 
 # Вывод параметров изображения и парогов восприятия
 print("Image: ", image.format, image.size, image.mode)
@@ -31,7 +31,8 @@ for i in range(0, image.size[0]):
         
         # Крашу линии
         pixel = image.getpixel((i, j))
-        pixelRight = image.getpixel((i, j + 1))
+        if j < image.size[1] - 1:
+            pixelRight = image.getpixel((i, j + 1))
         
         if abs(pixel[0] - pixelRight[0]) > pR: cmena = True
         if abs(pixel[1] - pixelRight[1]) > pG: cmena = True
@@ -43,6 +44,12 @@ for i in range(0, image.size[0]):
 
         # Пиксели цвета > (0, 0, 0) крашу в (255, 255, 255)
         if image.getpixel((i, j)) > (0, 0, 0): image.putpixel((i, j), (255, 255, 255))
+
+        if image.getpixel(i, j) == (0, 0, 0):
+            if i > 0: image.putpixel((i - 1, j), (0, 0, 0))
+            if i < image.size[0]: image.putpixel((i + 1, j), (0, 0, 0))
+            if j > 0: image.putpixel((i, j - 1), (0, 0, 0))
+            if j < image.size[1]: image.putpixel((i, j + 1), (0, 0, 0))
 
 # Сохранение и показ изображения
 image.save("tests/image11.png")
