fd9d251e8cece6ffe5bd5d34aba3e64d41b96cfb
Lev111111 <leviiloviee@gmail.com>
2022-06-27 09:50:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 from PIL import Image
 
-# Задаю переменную прогресса
-progress = 0
-progresDown = 0
+# Переменные смен цвета
+pixel = (0, 0, 0)
+pR = 0
+pG = 0
+pb = 0
 
 # Открытие изображениия
 # image_name = input("Print image name: ")
 image_name = "icon.jpg"
 image = Image.open(image_name)
 
+# Запрашиваю параметры парогов смены цвета
+pR = input("Parogue for R chnnale")
+
 # Вывод параметров изображения
 print("Image: ", image.format, image.size, image.mode)
 
 # Работа с изображением
 for i in range(0, image.size[0]):
-    # Работаю с прогрессом
-    progress += 1
-    # if progress % 10 == 0:
-    if progress != progresDown:
-        print("Porgress: ", round((progress * 100) / image.size[0], 0), "%")
-    progresDown = progress
-    
     for j in range(0, image.size[1]):
         # Пиксели цвета (0, 0, 0) крашу в (1, 1, 1)
         if image.getpixel((i, j)) == (0, 0, 0):
             image.putpixel((i, j), (1, 1, 1))
         
         # Крашу линии
-        if image.getpixel((i, j)) > (10, 10, 10) and image.getpixel((i, j)) < (90, 90, 90):
+        if image.getpixel((i, j)) > (10, 10, 10):
             image.putpixel((i, j), (0, 0, 0))
 
         # Пиксели цвета > (0, 0, 0) крашу в (255, 255, 255)
