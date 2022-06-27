3985f96a1bbc57b1e070d3065e241856e333c8fa
Lev111111 <leviiloviee@gmail.com>
2022-06-27 10:14:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from PIL import Image
 
 # Переменные смен цвета
 pixel = (0, 0, 0)
+pixelRight = (0, 0, 0)
+cmena = False
 pR = 0
 pG = 0
 pb = 0
@@ -25,17 +27,23 @@ print("Working with image")
 for i in range(0, image.size[0]):
     for j in range(0, image.size[1]):
         # Пиксели цвета (0, 0, 0) крашу в (1, 1, 1)
-        if image.getpixel((i, j)) == (0, 0, 0):
-            image.putpixel((i, j), (1, 1, 1))
+        if image.getpixel((i, j)) == (0, 0, 0): image.putpixel((i, j), (1, 1, 1))
         
         # Крашу линии
-        if image.getpixel((i, j)) > (10, 10, 10):
+        pixel = image.getpixel(i, j)
+        pixelRight = image.getpixel(i, j + 1)
+        
+        if abs(pixel[0] - pixelRight[0]) > pR: cmena = True
+        if abs(pixel[1] - pixelRight[1]) > pG: cmena = True
+        if abs(pixel[2] - pixelRight[2]) > pB: cmena = True
+
+        if cmena == True:
             image.putpixel((i, j), (0, 0, 0))
+            cmena = False
 
         # Пиксели цвета > (0, 0, 0) крашу в (255, 255, 255)
-        if image.getpixel((i, j)) > (0, 0, 0):
-            image.putpixel((i, j), (255, 255, 255))
+        if image.getpixel((i, j)) > (0, 0, 0): image.putpixel((i, j), (255, 255, 255))
 
 # Сохранение и показ изображения
-image.save("tests/image10.png")
+image.save("tests/image11.png")
 image.show()
