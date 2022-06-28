4c33fd224a35f6a500baaef67a77f50ebe78ce85
Lev111111 <leviiloviee@gmail.com>
2022-06-28 19:39:18 +0700

    Update Cvertka.java

--- Cvertka.java
+++ Cvertka.java
@@ -9,21 +9,46 @@ public class Cvertka {
         try {
             // Открываем изображение
             File file = new File(args[0]);
-            BufferedImage source = ImageIO.read(file);
+            BufferedImage image = ImageIO.read(file);
 
             // Создаем новое пустое изображение, такого же размера
-            BufferedImage result = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
+            BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
 
             // Делаем двойной цикл, чтобы обработать каждый пиксель
-            for (int x = 0; x < source.getWidth(); x++) {
-                for (int y = 0; y < source.getHeight(); y++) {
-            
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    // Получаем цвет текущего пикселя
+                    Color color = new Color(image.getRGB(x, y));
+
+                    // Получаем каналы этого цвета
+                    int blue = color.getBlue();
+                    int red = color.getRed();
+                    int green = color.getGreen();
+
+                    // Применяем стандартный алгоритм для получения черно-белого изображения
+                    int grey = (int) (red * 0.299 + green * 0.587 + blue * 0.114);
+
+                    // Если вы понаблюдаете, то заметите что у любого оттенка серого цвета, все каналы имеют
+                    // одно и то же значение. Так, как у нас изображение тоже будет состоять из оттенков серого
+                    // то, все канали будут иметь одно и то же значение.
+                    int newRed = grey;
+                    int newGreen = grey;
+                    int newBlue = grey;
+
+                    //  Cоздаем новый цвет
+                    Color newColor = new Color(newRed, newGreen, newBlue);
+
+                    // И устанавливаем этот цвет в текущий пиксель результирующего изображения
+                    result.setRGB(x, y, newColor.getRGB());
                 }
             }
+
+            // Созраняем результат в новый файл
+            File output = new File(args[1]);
+            ImageIO.write(result, "jpg", output);
         }
-        catch {
-            System.out.print("Fatal error:");
-            System.out.print("Неудается открыть или сохранить файл");
+        catch (IOException a) {
+            System.out.println("Can't read or save file"); // Or something more intellegent
         }
     }
 }
