400617025444c39f53680a3180222ad4f1078034
Lev111111 <leviiloviee@gmail.com>
2022-06-29 23:57:00 +0700

    Update Cvert.java

--- Cvert.java
+++ Cvert.java
@@ -35,19 +35,29 @@ public class Cvert {
                     for (int y = 0; y < res.getHeight(); y++) {
                         if (
                             (y > 0 & y < res.getHeight()) &
-                            (x > 0 & x < res.getWidth())
+                            (x < 0 & x < res.getWidth())
                         ) {
-                            // // System.out.println(res.getRGB(x - 1, y - 1));
-                            // System.out.println(res.getRGB(x - 1, y));
-                            // System.out.println(res.getRGB(x - 1, y + 1));
+                            znach[0][0] = res.getRGB(x - 1, y - 1);
+                            znach[0][1] = res.getRGB(x - 1, y);
+                            znach[0][2] = res.getRGB(x - 1, y + 1);
 
-                            // System.out.println(res.getRGB(x, y - 1));
-                            System.out.println(res.getRGB(x, y));
-                            // System.out.println(res.getRGB(x, y + 1));
+                            znach[1][0] = res.getRGB(x, y - 1);
+                            znach[1][1] = res.getRGB(x, y);
+                            znach[1][2] = res.getRGB(x, y + 1);
 
-                            // System.out.println(res.getRGB(x + 1, y - 1));
-                            // System.out.println(res.getRGB(x + 1, y));
-                            // System.out.println(res.getRGB(x + 1, y + 1));
+                            znach[2][0] = res.getRGB(x + 1, y - 1);
+                            znach[2][1] = res.getRGB(x + 1, y);
+                            znach[2][1] = res.getRGB(x + 1, y + 1);
+                        }
+                    }
+                    
+                    for (int xx = 0;  xx < 3; xx++) {
+                        for (int yy = 0; yy < 3; yy++) {
+                            if (yy < 2) {
+                                System.out.print(znach[xx][yy]);
+                            } else {
+                                System.out.println(znach[xx][yy]);
+                            }
                         }
                     }
                 }
