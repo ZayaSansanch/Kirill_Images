a04280dccc5a82c736df16a40985884b3339af51
Lev111111 <leviiloviee@gmail.com>
2022-06-29 22:41:58 +0700

    test

--- Cvertka.java
+++ Cvertka.java
@@ -5,6 +5,8 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.awt.*;
 import java.applet.*;
+import java.lang.Math;
+import java.lang.Integer;
 
 public class Cvertka {
     public static void main(String[] args) {
@@ -36,8 +38,10 @@ public class Cvertka {
                 }
             }
 
-            int black = new Color(100, 100, 100).getRGB();
-            int black2 = new Color(255, 255, 255).getRGB();
+            int black = new Color(0, 0, 0).getRGB();
+            int black2 = new Color(1, 1, 1).getRGB();
+            int white = new Color(255, 255, 255).getRGB();
+            int red = new Color(255, 0, 0).getRGB();
 
             for (int x = 0; x < image.getWidth(); x++) {
                 for (int y = 0; y < image.getHeight(); y++) {
@@ -46,21 +50,32 @@ public class Cvertka {
                 }
             }
             
+            int pixel = 0;
+            int pixelRight = 0;
+            int pixelminpixelrightABS = 0;
+            boolean cmena = false;
+            
             for (int x = 0; x < image.getWidth(); x++) {
                 for (int y = 0; y < image.getHeight(); y++) {
-                    if (Resdown.getRGB(x, y) < black) {
-                        Resdown.setRGB(x, y, black2);
+                    if (Resdown.getRGB(x, y) == black) {Resdown.setRGB(x, y, black2);}
+
+                    pixel = Resdown.getRGB(x, y);
+                    if (y < image.getWidth() - 1) {pixelRight = Resdown.getRGB(x, y + 1);}
+
+                    pixelminpixelrightABS = Math.abs(pixel - pixelRight);
+                    if (pixelminpixelrightABS > Integer.parseInt(args[1])) {cmena = true;}
+                    if (cmena == true) {
+                        Resdown.setRGB(x, y, red);
+                        cmena = false;
                     }
 
-                    // Color color = new Color(image.getRGB(x, y));
-                    // int red = color.getRed();
-                    // int green = color.getGreen();
-                    // int grey = (int) (red * 0.299 + green * 0.587 + blue * 0.114);
-                    // int newRed = grey;
-                    // int newGreen = grey;
-                    // int newBlue = grey;
-                    // Color newColor = new Color(newRed, newGreen, newBlue);
-                    // ResBW.setRGB(x, y, newColor.getRGB());
+                    if (Resdown.getRGB(x, y) != red) {Resdown.setRGB(x, y, white);}
+                }
+            }
+
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    if (Resdown.getRGB(x, y) == red) {Resdown.setRGB(x, y, black);}
                 }
             }
 
