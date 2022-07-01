d4cfc0680e7402bf6fc81c052a39ae0ca9ad8cd6
Lev111111 <leviiloviee@gmail.com>
2022-07-01 09:40:21 +0700

    Create many IMG method

--- Cvert.java
+++ Cvert.java
@@ -7,9 +7,10 @@ import java.lang.Math;
 // import java.lang.Integer;
 
 public class Cvert {
-    public static void main(String[] args) {
+    void iteretionFunction(String OFN, String SFN, int num) {
+        // OFN = opening file name; SFN = saving file name;
         try {
-            File file = new File(args[0]);
+            File file = new File(OFN);
             BufferedImage image = ImageIO.read(file);
             double wid = Math.ceil(image.getWidth() / 3);
             double heg = Math.ceil(image.getHeight() / 3);
@@ -27,48 +28,62 @@ public class Cvert {
                 {0, 0, 0}
             };
 
-            // int k = 0;
-            // while (k < 11) {
-                // System.out.println("Iteretion: " + k);
-                // k++;
+            int k = 0;
+            while (k < 1) {
+                System.out.println("Iteretion: " + k);
+                k++;
                 for (int x = 0;  x < res.getWidth(); x++) {
                     for (int y = 0; y < res.getHeight(); y++) {
                         if (
                             (y > 0 & y < res.getHeight() - 1) &
                             (x > 0 & x < res.getWidth() - 1)
                         ) {
-                            znach[0][0] = res.getRGB(x - 1, y - 1);
-                            znach[0][1] = res.getRGB(x - 1, y);
-                            znach[0][2] = res.getRGB(x - 1, y + 1);
+                            znach[0][0] = res.getRGB(x - 1, y - 1);    znach[0][1] = res.getRGB(x - 1, y);    znach[0][2] = res.getRGB(x - 1, y + 1); 
+                            znach[1][0] = res.getRGB(x, y - 1);        znach[1][1] = res.getRGB(x, y);        znach[1][2] = res.getRGB(x, y + 1);
+                            znach[2][0] = res.getRGB(x + 1, y - 1);    znach[2][1] = res.getRGB(x + 1, y);    znach[2][2] = res.getRGB(x + 1, y + 1);
 
-                            znach[1][0] = res.getRGB(x, y - 1);
-                            znach[1][1] = res.getRGB(x, y);
-                            znach[1][2] = res.getRGB(x, y + 1);
+                            for (byte mx = 0; mx < 3; mx++) {
+                                for (byte my = 0; my < 3; my++) {
+                                    if (mx != 1 & my != 1) {
+                                        znach[mx][my] *= -1;
+                                    } else {
+                                        znach[mx][my] *= num;
+                                    }
+                                }
+                            }
 
-                            znach[2][0] = res.getRGB(x + 1, y - 1);
-                            znach[2][1] = res.getRGB(x + 1, y);
-                            znach[2][1] = res.getRGB(x + 1, y + 1);
+                            res.setRGB(x - 1, y - 1, znach[0][0]);    res.setRGB(x - 1, y, znach[0][1]);    res.setRGB(x - 1, y + 1, znach[0][2]); 
+                            res.setRGB(x, y - 1, znach[1][0]);    res.setRGB(x, y, znach[1][1]);    res.setRGB(x, y + 1, znach[1][2]); 
+                            res.setRGB(x + 1, y - 1, znach[2][0]);    res.setRGB(x + 1, y, znach[2][1]);    res.setRGB(x + 1, y + 1, znach[2][2]); 
                         }
                     }
                     
-                    for (int xx = 0;  xx < 3; xx++) {
-                        for (int yy = 0; yy < 3; yy++) {
-                            if (yy < 2) {
-                                System.out.print(znach[xx][yy] + " ");
-                            } else {
-                                System.out.println(znach[xx][yy]);
-                            }
-                        }
-                    }
+                    // for (int xx = 0;  xx < 3; xx++) {
+                    //     for (int yy = 0; yy < 3; yy++) {
+                    //         if (yy < 2) {
+                    //             System.out.print(znach[xx][yy] + " ");
+                    //         } else {
+                    //             System.out.println(znach[xx][yy]);
+                    //         }
+                    //     }
+                    // }
                 }
 
-            // }
+            }
 
-            File cvert = new File("cvert.jpg");
+            File cvert = new File(SFN);
             ImageIO.write(res, "jpg", cvert);
 
         } catch (IOException e) {
             System.out.println("Can't read or save");
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            Cvert cvert = new Cvert();
+
+            cvert.iteretionFunction(args[0], "finished/" + i + ".jpg", i);
         }
     }
 }
